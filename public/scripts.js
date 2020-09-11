719cb0da1d7422bed1b9541c15aefd3e31324402
Felipe Joazeiro <felipejoazeiro@gmail.com>
2020-09-10 21:53:29 -0300

    Mask do cep e cpf prontos

--- public/scripts.js
+++ public/scripts.js
@@ -10,6 +10,37 @@ const Mask = {
             style: 'currency',
             currency:'BRL'
         }).format(value/100)
+    },
+    cpfCnpj(value){
+        value = value.replace(/\D/g,"")
+
+        if(value.length > 14){
+            value = value.slice(0, -1)
+        }
+        //check if is cpf or cnpj
+        if(value.length > 11){
+            value = value.replace(/(\d{2})(\d)/, "$1.$2")
+
+            value = value.replace(/(\d{3})(\d)/, "$1.$2")
+
+            value = value.replace(/(\d{3})(\d)/, "$1/$2")
+
+            value = value.replace(/(\d{4})(\d)/, "$1-$2")
+        }else{
+            value = value.replace(/(\d{3})(\d)/, "$1.$2")
+            value = value.replace(/(\d{3})(\d)/, "$1.$2")
+            value = value.replace(/(\d{3})(\d)/, "$1-$2")
+
+        }
+        return value
+    }, 
+    cep(value){
+        value = value.replace(/\D/g,"")
+        value = value.replace(/(\d{5})(\d)/, "$1-$2")
+        if(value.length > 9){
+            value = value.slice(0, -1)
+        }
+        return value
     }
 } 
 
